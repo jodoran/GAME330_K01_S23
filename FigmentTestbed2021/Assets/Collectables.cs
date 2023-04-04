46082457f8ce88c3ae24ea9a7c5adf576547357a
Swords-Oath <jxraphael@gmail.com>
2023-04-04 16:27:24 +0900

    Collectables, demo art, & player modifications
    Changed the health to a slider, and further adjusted the player movement for smoother gameplay. Added collectables, but they don't do anything yet. Added demo art to make the scene look a bit nicer until the art is finished.

---
+++ FigmentTestbed2021/Assets/Collectables.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    public float _maxY;
+    public float _minY;
+    bool UpDown;
+
+    private void Update()
+    {
+        if(transform.position.y >= _maxY)
+        {
+            UpDown = false;
+        }
+        else if (transform.position.y <= _minY)
+        {
+            UpDown = true;
+        }
+
+        if(UpDown == true)
+        {
+            transform.Translate((Vector3.up * Time.deltaTime) / 2, Space.Self);
+        }
+        else if (UpDown == false)
+        {
+            transform.Translate((Vector3.down * Time.deltaTime) / 2, Space.Self);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
