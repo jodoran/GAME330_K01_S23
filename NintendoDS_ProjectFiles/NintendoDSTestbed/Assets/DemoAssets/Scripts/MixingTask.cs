fb6bde64f0646e541154c8a1d9ff4bf21525a5e5
Swords-Oath <jxraphael@gmail.com>
2023-05-19 12:53:24 +0900

    Improved Mixing
    Improved Mixing task.

--- NintendoDS_ProjectFiles/NintendoDSTestbed/Assets/DemoAssets/Scripts/MixingTask.cs
+++ NintendoDS_ProjectFiles/NintendoDSTestbed/Assets/DemoAssets/Scripts/MixingTask.cs
@@ -6,20 +6,26 @@ using UnityEngine.UI;
 public class MixingTask : MonoBehaviour, IDSTapListener
 {
     public Image Point;
+    public Slider QualityBar;
+    public Slider ProgressBar;
 
     public Vector3 MixingPoint;
     List<Vector3> PointList = new List<Vector3>();
-    public int PointCount = 2;
+
+    public int Cycles = 1;
     public int CircleRadius = 5;
     public int Points;
+    public float MinQuality;
+
     int PointsClicked = 0;
-    float Progress = 0;
+    float Progress = 1;
+    float Quality = 0;
 
     private void Start()
     {
         float angle = 0;
-        float AngleIntervals = 360/ Points ;
-        while (PointList.Count < Points)
+        float AngleIntervals = 360/ Points;
+        while (PointList.Count < Points * Cycles)
         {
             Vector3 nextPoistion;
             float x = CircleRadius * Mathf.Cos(angle);
@@ -32,6 +38,24 @@ public class MixingTask : MonoBehaviour, IDSTapListener
         SetPoint();
     }
 
+    private void Update()
+    {
+        if (Quality > 0)
+        {
+            Quality -= Time.deltaTime / 5f;
+            QualityBar.value = Quality;
+
+            if (Quality >= .9f)
+            {
+                Point.color = Color.green;
+            }
+            else
+            {
+                Point.color = Color.red;
+            }
+        }
+    }
+
     public void OnScreenTapDown(Vector2 tapPosition)
     {
 
@@ -40,13 +64,16 @@ public class MixingTask : MonoBehaviour, IDSTapListener
             if (PointsClicked < PointList.Count)
             {
                 SetPoint();
+                Quality = 1;
+                ModifyProgress();
             }
-            else
+            else if(PointsClicked == PointList.Count)
             {
                 Point.enabled = false;
+                ModifyProgress();
+                QualityBar.gameObject.SetActive(false);
             }
         }
-        
     }
 
     public void OnScreenDrag(Vector2 tapPosition)
@@ -60,13 +87,24 @@ public class MixingTask : MonoBehaviour, IDSTapListener
     }
 
 
+    void ModifyProgress()
+    {
+        if(QualityBar.value < MinQuality)
+        {
+            float progressBars = 100/ (PointList.Count - 1);
+            Progress -= progressBars / 100;
+            Debug.Log(progressBars);
+        }
+        ProgressBar.value = Progress;
+    }
+
     void SetPoint()
     {
         Vector3 currPosition = Point.rectTransform.anchoredPosition;
         currPosition.x += (PointList[PointsClicked].x - currPosition.x);
         currPosition.y += (PointList[PointsClicked].y - currPosition.y);
         Point.rectTransform.anchoredPosition = currPosition;
+
         PointsClicked++;
-        Progress += PointList.Count / 100;
     }
 }
