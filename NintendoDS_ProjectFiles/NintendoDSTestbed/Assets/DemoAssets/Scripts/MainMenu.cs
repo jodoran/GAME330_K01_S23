f82ec18e15dbc37b2607670f5653588188faa2b1
Swords-Oath <jxraphael@gmail.com>
2023-05-21 14:17:15 +0900

    Added Screen Interaction with Menu Scene
    Made the Play and Quit functions called when their respective buttons are pressed on the screen.

--- NintendoDS_ProjectFiles/NintendoDSTestbed/Assets/DemoAssets/Scripts/MainMenu.cs
+++ NintendoDS_ProjectFiles/NintendoDSTestbed/Assets/DemoAssets/Scripts/MainMenu.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour, IDSTapListener
 {
+    //Used to get the positions of the Buttons
+    public RectTransform PlayButton;
+    public RectTransform QuitButton;
+
+    // Called Once when Screen is tapped down on
+    public void OnScreenTapDown(Vector2 tapPosition)
+    {
+        //Checks the position of the button and compares it to the tapped position
+        if (DSTapRouter.RectangleContainsDSPoint(PlayButton, tapPosition))
+        {
+            Play(); //Calls Play if Play Button is tapped on
+        }
+        else if (DSTapRouter.RectangleContainsDSPoint(QuitButton, tapPosition))
+        {
+            Quit(); //Calls Quit if Quit Button is tapped on
+        }
+    }
+
+    // Called when continously as Screen is dragged on
+    public void OnScreenDrag(Vector2 tapPosition) { }
+
+    // Called Once when the tap is lifted up from the Screen 
+    public void OnScreenTapUp(Vector2 tapPosition) { }
+
+
+
     // Load Scene
-    public void play()
+    public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
